8561f730f19b42caca3f8857a96b98316866cb60
tanquanbui <tanquanbui@gmail.com>
2022-12-20 19:52:29 +0700

    second build

---
+++ main-app/build/precache-manifest.df64ccb0a9ff673dc0f3f2d6244efa6e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f8ce87241580a5e312f",
+    "url": "/static/css/main.6881089a.chunk.css"
+  },
+  {
+    "revision": "1f8ce87241580a5e312f",
+    "url": "/static/js/main.1f8ce872.chunk.js"
+  },
+  {
+    "revision": "d5c02a97694c72e90752",
+    "url": "/static/js/1.d5c02a97.chunk.js"
+  },
+  {
+    "revision": "082ade178d485dc2d716",
+    "url": "/static/js/2.082ade17.chunk.js"
+  },
+  {
+    "revision": "78778d6f2d53e260cb95",
+    "url": "/static/js/runtime~main.78778d6f.js"
+  },
+  {
+    "revision": "ac04bf225fe482adaa835b5e68a9d6a9",
+    "url": "/index.html"
+  }
+];
